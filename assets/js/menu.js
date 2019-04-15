ed707e7c9f53364a97df5b01a6601883a927b8fa
Kan Adachi <kadachi1023@gmail.com>
2019-04-14 17:36:35 -0700

    Adds responsive sidebar

---
+++ assets/js/menu.js
@@ -0,0 +1,24 @@
+(function() {
+
+const ui = {
+  menu: null,
+  sidebarContent: null,
+};
+
+function handleHamburgerClick() {
+  ui.sidebarContent.classList.toggle('active');
+}
+
+function initSelectors() {
+  ui.menu = document.querySelector('.hamburger-menu');
+  ui.sidebarContent = document.querySelector('.sidebar-content');
+}
+
+function init() {
+  initSelectors();
+  ui.menu.addEventListener('click', handleHamburgerClick);
+};
+
+window.addEventListener('load', init);
+
+})();
